9dab3cf54d7292db79066f37bfa5da26b162a84a
nazhou <zhou.na@northeastern.edu>
2024-11-22 21:02:48 -0800

    A5 FINISHED

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,17 +1,24 @@
 import * as modulesDao from "./dao.js";
+
+const findModulesForCourse = async (req, res) => {
+  const { courseId } = req.params;
+  const modules = await modulesDao.findModulesForCourse(courseId);
+  res.json(modules);
+};
+
 export default function ModuleRoutes(app) {
- app.delete("/api/modules/:moduleId", (req, res) => {
-   const { moduleId } = req.params;
-   modulesDao.deleteModule(moduleId);
-   res.sendStatus(204);
-}); 
-app.put("/api/modules/:moduleId", (req, res) => {
+  app.delete("/api/modules/:moduleId", (req, res) => {
+    const { moduleId } = req.params;
+    modulesDao.deleteModule(moduleId);
+    res.sendStatus(204);
+  }); 
+
+  app.put("/api/modules/:moduleId", (req, res) => {
     const { moduleId } = req.params;
     const moduleUpdates = req.body;
     modulesDao.updateModule(moduleId, moduleUpdates);
     res.sendStatus(204); 
-});
+  });
 
-
-
+  app.get("/api/courses/:courseId/modules", findModulesForCourse);
 }
